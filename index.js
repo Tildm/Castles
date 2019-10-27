e85cfe7da8f0a21d2f353d2688cb6f7f6cb60296
Tildm <til_takacs@yahoo.com>
2019-10-27 20:15:13 +0100

    initial try

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ var app = express();
 var cors = require("cors");
 // port =  8080
 bodyParser = require("body-parser");
-// var cors = require("cors");
+var cors = require("cors");
 
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Istvan:Depeche75@cluster0-5d6il.mongodb.net/test?retryWrites=true&w=majority', {
-	useNewUrlParser: true,
-	useCreateIndex: true
-}).then(() => {
-	console.log('Connected to the atlas DB!');
-}).catch(err => {
-	console.log('ERROR:', err.message);
-});
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://Istvan:Depeche75@cluster0-5d6il.mongodb.net/test?retryWrites=true&w=majority', {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true
+// }).then(() => {
+// 	console.log('Connected to the atlas DB!');
+// }).catch(err => {
+// 	console.log('ERROR:', err.message);
+// });
 
+var PORT = 4000;
 var PORT = process.env.PORT || 4000;
+
 
 // port = 3000
 // var port = process.env.PORT || 3000;
@@ -24,7 +26,7 @@ var PORT = process.env.PORT || 4000;
 
 var castleRoutes = require("./routes/castles")
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
